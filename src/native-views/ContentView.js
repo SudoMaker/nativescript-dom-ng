7169f2a6b29a10342442d65004c8a1529c6b1935
Yukino Song <yukino@yuki.network>
2023-01-19 21:32:48 +0800

    Add support for defining children handling

--- src/native-views/ContentView.js
+++ src/native-views/ContentView.js
@@ -1,26 +1,10 @@
 import { ContentView } from '@nativescript/core'
 import { named, makeView } from './mixin.js'
+import { mergeProps } from '../utils.js'
 
 export const makeContentView = /*#__PURE__*/named(
 	'ContentView', 'ContentView', ContentView,
-	(_, options) => class ContentViewElement extends /*#__PURE__*/makeView(_, options) {
-		__dominative_onInsertChild(child, ref) {
-			if (!child.__dominative_isNative || (ref && !ref.__dominative_isNative)) return super.__dominative_onInsertChild(child, ref)
-
-			if (this.content && this.content.__dominative_isNative) this.content.remove()
-			this.content = child
-
-			super.__dominative_onInsertChild(child, ref)
-		}
-
-		__dominative_onRemoveChild(child) {
-			if (!child.__dominative_isNative) return super.__dominative_onRemoveChild(child)
-
-			this.content = null
-
-			super.__dominative_onRemoveChild(child)
-		}
-	}
+	(_, options) => class ContentViewElement extends /*#__PURE__*/makeView(_, mergeProps({childrenPolicy: 'content'}, options)) {}
 )
 
 export default /*#__PURE__*/makeContentView.master()
